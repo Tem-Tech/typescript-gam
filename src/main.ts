74ee300fafa8ea7ae036446ac7056da7ea400d2e
Tem-Tech <96115464+Tem-Tech@users.noreply.github.com>
2024-08-28 20:44:01 +0100

    Game homepage done, on click game board appears

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const player1: string = "P";
+const player2: string = "B";
+let currentPlayer: string = player1;
+let gameOver: boolean = false;
+let board: string[][]; // Define the board as a 2D array of strings
+const rows: number = 6;
+const columns: number = 7;
+var button: button = document.getElementById("start");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//click button to start game and display board
+button?.addEventListener("click",setGame);
+// Initialize the board as an empty array
+function setGame(): void {
+  const game = document.createElement("div");
+  game.id = "board"
+  document.body.appendChild(game);
+
+  board = []; 
+// Create a new row array for each iteration
+
+  for (let r = 0; r < rows; r++) {
+    let row: string[] = []; 
+    for (let c = 0; c < columns; c++) {
+      // Initialize each cell in the row with an empty space
+      row.push(' '); 
+      // Create a new tile (div) for each cell
+      const tile = document.createElement("div");
+      // Set the ID for the tile using the current row and column indices
+      tile.id = r.toString() + "-" + c.toString(); 
+      //display tiles
+      tile.classList.add("tile");
+      // Append the tile to the board element in the DOM if it exists
+      document.getElementById("board")?.append(tile);
+    }
+// Add the created row to the board array
+    board.push(row); 
+  }
+}
